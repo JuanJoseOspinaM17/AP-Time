b7c0dae40140677422cd98cc01c25a85a97345c5
Luis Alfredo Rodriguez Rodriguez <rodriguezzluisalfredo@gmail.com>
2026-09-11 19:08:22 -0500

    Configure Vercel and external DB env variables

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'ap_time';
-$user = 'root';
-$pass = '';
+$host = getenv('DB_HOST') ?: 'localhost';
+$db = getenv('DB_NAME') ?: 'ap_time';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_EMULATE_PREPARES=>false];
